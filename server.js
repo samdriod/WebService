ceee64b4d96e24cc72e068bc430e8911fafb84a3
Sam <skarki48@myseneca.ca>
2025-01-15 21:32:40 -0500

    fix: use process.env.PORT for HTTP_PORT configuration

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const ListingsDB = require("./modules/listingsDB.js");
 const db = new ListingsDB();
 
 const app = express();
-const HTTP_PORT = process.PORT || 3080;
+const HTTP_PORT = process.env.PORT || 3080;
 app.use(cors());
 app.use(express.json());
 
